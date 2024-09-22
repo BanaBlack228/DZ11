2893132c9b59554e04f3b1fdffcb2db8d5c09dd0
BanaBlack228 <BanaBlack228>
2024-09-22 21:44:50 +0300

    1

---
+++ js/1.js
@@ -0,0 +1,39 @@
+let car = {
+    manufacturer: "Rolls-Royse",
+    model: "Ghost",
+    year: 2009,
+    AverageSpeed: 150
+};
+
+function Info(obj) {
+    let INF = "Производитель: " + obj.manufacturer + "\n"
+        + "Модель: " + obj.model + "\n"
+        + "Год выпуска: " + obj.year + " г\n"
+        + "Средняя скорость: " + obj.averageSpeed + " км/ч";
+    alert(INF);
+}
+
+function Travel() {
+
+    let distance = prompt("Введите расстояние (км): ");
+
+    let time = Time(distance)
+
+    alert("Средняя скорость: " + car.AverageSpeed + " км/ч\n"
+        + "Расстояние: " + distance + " км\n"
+        + "Время для преодоления: " + time + " ч");
+}
+
+function Time(distance) {
+    let t = Math.round(
+        (distance / car.AverageSpeed) * 100
+        ) / 100;
+    let ost = t % 1;
+
+    ost = ost * 0.6;
+
+    t = Math.floor(t) + ost;
+    
+    var BreakTime = Math.floor(t / 4);
+    return (t + BreakTime).toFixed(2);
+}
